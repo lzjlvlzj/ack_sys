967328bd3510bd4140496a9b9b6b2f7101956460
lzjlvlzj <mxdyxzy@163.com>
2017-07-12 15:35:31 +0800

    project controller

--- ack-admin-web/src/main/java/org/ack/admin/web/controller/ProjectController.java
+++ ack-admin-web/src/main/java/org/ack/admin/web/controller/ProjectController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * @author GeoStar
+ * @author ack
  *
  */
 @Controller
