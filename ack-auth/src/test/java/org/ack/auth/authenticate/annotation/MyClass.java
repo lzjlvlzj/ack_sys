07312fcc36d3ed86a091c57fb8d8b0195768fe5f
lzjlvlzj <mxdyxzy@163.com>
2019-12-18 17:35:20 +0800

    spring boot init

--- ack-auth/src/test/java/org/ack/auth/authenticate/annotation/MyClass.java
+++
@@ -1,9 +0,0 @@
-package org.ack.auth.authenticate.annotation;
-
-public class MyClass {
-	
-	@AckPermission("user:list or user:find")
-    public void myMethod(){
-    	System.out.println(1);
-    }
-}
