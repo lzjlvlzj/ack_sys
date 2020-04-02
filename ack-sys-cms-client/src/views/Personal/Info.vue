974732f9af94a3f91e182b2d224bb1d981de4edd
lzjlvlzj <mxdyxzy@163.com>
2020-04-02 10:17:41 +0800

    css

--- ack-sys-cms-client/src/views/Personal/Info.vue
+++ ack-sys-cms-client/src/views/Personal/Info.vue
@@ -78,35 +78,7 @@
 </template>
 
 <style scoped>
-  .data-form {
-    width: 80%;
-    text-align: left;
-  }
-  .pc-avatar-uploader {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-    width: 178px;
-    height: 178px;
-  }
-  .pc-avatar-uploader:hover {
-    border-color: #409EFF;
-  }
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px;
-    text-align: center;
-  }
-  .avatar {
-    width: 178px;
-    height: 178px;
-    display: block;
-  }
+
 </style>
 
 <script>
